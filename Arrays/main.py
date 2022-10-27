e7ff42b9bc3786a70919c5b92d7019c369339606
Abdihakim Muhumed <64124679+Abdihakim-Muhumed@users.noreply.github.com>
2022-10-27 10:28:32 +0300

    Creating Arrays

---
+++ Arrays/main.py
@@ -0,0 +1,10 @@
+# Creating an array
+# 1. Assign it to a variable
+# 2. Define type of elements
+# 3. Define its size
+# NB: Import array module for python 
+
+from array import *
+arr1 = array("i",[1,2,3,4,5,6])
+arr2 = array("d",[1,2, 3.5, 5.6])
+print(arr1)
