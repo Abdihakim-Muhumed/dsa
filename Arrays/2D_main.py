33c6a825aa2859a21d291dc0155fb29853ea2b18
Abdihakim Muhumed <64124679+Abdihakim-Muhumed@users.noreply.github.com>
2022-10-30 10:52:54 +0300

    Inserting into a 2 D

--- Arrays/2D_main.py
+++ Arrays/2D_main.py
@@ -1,6 +1,7 @@
 # Created by Abdihakim Muhumedon 30/10/2022
 # Copyright @ 2022 Abdihakim. All rights reserved.
 
+# Two Dimensional Array
 """ Day 1 - 11, 15, 10, 6
     Day 2 - 10, 14, 11, 5
     Day 3 - 12, 17, 12, 8
@@ -10,3 +11,7 @@ import numpy as np
 
 twoDArray = np.array([[11, 15, 10, 6], [10, 14, 11, 5], [12, 17, 12, 8], [15, 18, 14, 9]])
 print(twoDArray)
+
+# Inserting -  Two Dimensional Array
+newTwoDArray = np.insert(twoDArray, 1,[[1,2,3,4]], axis=1)
+print(newTwoDArray)
