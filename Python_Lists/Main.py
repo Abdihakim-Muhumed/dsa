30411e1765c024456bc449055dc624d17d477ce4
Abdihakim Muhumed <64124679+Abdihakim-Muhumed@users.noreply.github.com>
2022-11-01 06:32:36 +0300

    updating, iserting,slicing and deleting elements in a list

--- Python_Lists/Main.py
+++ Python_Lists/Main.py
@@ -17,3 +17,21 @@ for item in shoppingList:
     print(item)
 for i in range(len(shoppingList)):
     print(i,'. ' + shoppingList[i])
+    
+
+#update/insert a list
+myList = [1,2,3,4,5,6,7]
+print(myList)
+myList[2] = 33 # updating
+myList.insert(0, 11) # inserting
+print(myList)
+
+# Slicing, Deleting in a list
+myList = ['a', 'v', 'b','c', 'd', 'e']
+print(myList[0:2]) #slicing
+#myList.pop(1)
+#del myList[3] 
+#del myList[2:4]
+myList.remove('e')
+print(myList)
+
